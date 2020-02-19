2e2b55e7bcf17c0d127b96ddc107bbefeee09ae0
SuhailG <github@suhailg.me>
2020-02-19 15:10:41 -0700

    Revert "Update add liquidity address from v2 to v3"
    This reverts commit a47aba6a31fec313eeb9f383e41defe96f80b18a.

--- services/addressService.js
+++ services/addressService.js
@@ -1,7 +1,7 @@
 // Zaps whose volume should be aggregated among all contracts may have multiple addresses
 
-const UniSwap_AddLiquidityV2_General = '0x97402249515994Cc0D22092D3375033Ad0ea438A'
-const UniSwapRemoveLiquidityGeneral_v1 = '0x4316e3aD83ca2Cf0ea5e3b25e3DE2fA7F93cfE9c'
+const UniSwap_AddLiquityV2_General = '0x606563f8DC27F316b77F22d14D9Cd025B4F70469'
+const UniSwapRemoveLiquityGeneral_v1 = '0x4316e3aD83ca2Cf0ea5e3b25e3DE2fA7F93cfE9c'
 const proxy = '0x52fc6455f258760705e70f70160b06619bfe0adb'
 
 const unipoolAddresses = [
@@ -165,9 +165,9 @@ const getUnipoolAddresses = () => unipoolAddresses
 
 const getClassicAddresses = () => classicAddresses
 
-const getUniswapGeneral = () => UniSwap_AddLiquidityV2_General
+const getUniswapGeneral = () => UniSwap_AddLiquityV2_General
 
-const getUniSwapRemoveGeneral = () => UniSwapRemoveLiquidityGeneral_v1
+const getUniSwapRemoveGeneral = () => UniSwapRemoveLiquityGeneral_v1
 
 const getProxy = () => proxy
 
