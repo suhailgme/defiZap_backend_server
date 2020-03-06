41d0275bdbdb29dc457135aa51633baf498291f6
SuhailG <github@suhailg.me>
2020-03-06 03:03:04 -0700

    Separate Aggregate Stats and Zap Out Transaction List

--- routes/zapOuts.js
+++ routes/zapOuts.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const zapOuts = require('../services/zapOuts')
-router.get('/', async (req, res, next) =>{
-    res.json(zapOuts())
+router.get(['/'], async (req, res, next) => {
+  if (req.query.list) res.json(zapOuts().zapOuts)
+  else res.json(zapOuts().aggregateZapOuts)
 })
 
 module.exports = router
